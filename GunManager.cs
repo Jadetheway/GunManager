ec880081c8e1ee8f6d959b337e6979f3718f64a3
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2023-02-25 08:22:16 -0500

    Update to have naming be more consistent

--- GunManager.cs
+++ GunManager.cs
@@ -4,30 +4,31 @@ using UnityEngine;
 
 public class GunManager : MonoBehaviour
 {
+    //Naming consisitency. You don't need to follow the Microsoft naming convention but you should be consistent
     public List<GameObject> Guns = new List<GameObject>();
 
 
-    public Transform gunPos;
-    private int startNumber;
-    private GameObject currentGun;
-    public GameObject startGun;
-    private GameObject ActiveGun;
+    public Transform GunPos;
+    private int _startNumber;
+    private GameObject _currentGun;
+    public GameObject StartGun;
+    private GameObject _activeGun;
 
-    int index;
-    private bool hasAGun;
+    int _index;
+    private bool _hasAGun;
 
     public PickupController PickupController;
 
     // Start is called before the first frame update
     void Start()
     {
-        Guns.Add(startGun);
-        currentGun = Instantiate(Guns[index], gunPos.transform.position, gunPos.transform.rotation);
-        index = 0;
-        currentGun = Guns[index];
-        hasAGun = true;
-        currentGun.transform.position = gunPos.transform.position;
-        currentGun.transform.rotation = gunPos.transform.rotation;
+        Guns.Add(StartGun);
+        _currentGun = Instantiate(Guns[_index], GunPos.transform.position, GunPos.transform.rotation);
+        _index = 0;
+        _currentGun = Guns[_index];
+        _hasAGun = true;
+        _currentGun.transform.position = GunPos.transform.position;
+        _currentGun.transform.rotation = GunPos.transform.rotation;
     }
 
     // Update is called once per frame
@@ -38,29 +39,29 @@ public class GunManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.J) && Guns.Count > 1)
         {
 
-            index += 1;
-            Destroy(currentGun.gameObject);
-            ActiveGun = Instantiate(Guns[index], gunPos.transform.position, gunPos.transform.rotation);
+            _index += 1;
+            Destroy(_currentGun.gameObject);
+            _activeGun = Instantiate(Guns[_index], GunPos.transform.position, GunPos.transform.rotation);
 
         }
-        if (index > Guns.Capacity)
+        if (_index > Guns.Capacity)
         {
-            index = 1;
+            _index = 1;
         }
 
      
-            currentGun = Guns[index];
-            currentGun.transform.position = gunPos.transform.position;
-            currentGun.transform.rotation = gunPos.transform.rotation;
+            _currentGun = Guns[_index];
+            _currentGun.transform.position = GunPos.transform.position;
+            _currentGun.transform.rotation = GunPos.transform.rotation;
         
 
-        Debug.Log(index);
+        Debug.Log(_index);
     }
     public void NewObjectCollected(GameObject ObjectCollected)
     {
         Guns.Add(ObjectCollected.gameObject);
-        Destroy(currentGun.gameObject);
-        index += 1;
-        currentGun = Instantiate(Guns[index], gunPos.transform.position, gunPos.transform.rotation);
+        Destroy(_currentGun.gameObject);
+        _index += 1;
+        _currentGun = Instantiate(Guns[_index], GunPos.transform.position, GunPos.transform.rotation);
     }
 }
